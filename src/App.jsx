9ceeae03eccdf1f4a80f50360ed8556b7819ec0e
Pedro Silva <pedrosilvacwb@gmail.com>
2022-10-08 08:47:30 -0300

    add: add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import styles from './style';
 
-const App = () => {
-  return <div></div>;
-};
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  NavBar,
+  Stats,
+  Testimonials,
+} from './components';
 
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{' '}
+        <Clients /> <CTA /> <Footer />
+      </div>
+    </div>
+  </div>
+);
 export default App;
