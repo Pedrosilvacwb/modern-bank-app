5bcdcd6cc6b52032db573e2e6793c608950f0235
Pedro Silva <pedrosilvacwb@gmail.com>
2022-12-26 19:52:24 -0300

    fix: fixing some bugs

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { clients } from '../constants';
+import styles from '../style';
 
 const Clients = () => {
-  return <div></div>;
+  return (
+    <section className={`${styles.flexCenter} m-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => {
+          return (
+            <div
+              className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-2-[120px]`}
+              key={client.id}
+            >
+              <img
+                className="sm:2-[192px] w-[100px] object-contain"
+                src={client.logo}
+                alt="client"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { quotes } from '../assets';
 
-const FeedBackCard = () => {
-  return <div></div>;
+const FeedBackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        className="w-[42px] h-[27px] object-contain"
+        src={quotes}
+        alt="double_quote"
+      />
+      <p className="font-poppins font-normal text-[18px] leading-[32px]  text-white my-10">
+        {content}
+      </p>
+
+      <div className="flex flex-row">
+        <img className="w-[48px] h-[48px] rounded-full" src={img} alt={name} />
+
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
+            {name}
+          </h4>
+          <p className="font-poppins font-normal text-[16px] leading-[24px]  text-white">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedBackCard;
